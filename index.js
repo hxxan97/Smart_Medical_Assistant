ec4179a1616fb37ba53098f0f3028e0cfb60ca6c
hxxan97 <63323072+hxxan97@users.noreply.github.com>
2020-07-29 11:01:34 -0700

    added Smart_Medical_Assistant

---
+++ index.js
@@ -0,0 +1,7 @@
+$("button.download-button").click(function redirect(){
+  window.location.assign("https://drive.google.com/drive/folders/1_Xgpr0Trv3uE1iZEpAdQngiC5niZz9mg?usp=sharing")
+});
+
+$("button.btn-outline-dark").click(function goto(){
+  window.location.assign("https://mail.google.com")
+});
