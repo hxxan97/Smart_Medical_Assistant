5189df84daff1cceb4ff7a41a8b395f1b49f2e15
hxxan97 <63323072+hxxan97@users.noreply.github.com>
2020-08-11 11:01:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $("button.download-button").click(function redirect(){
-  window.location.assign("https://drive.google.com/drive/folders/1_Xgpr0Trv3uE1iZEpAdQngiC5niZz9mg?usp=sharing")
+  window.location.assign("https://drive.google.com/drive/folders/1ayC9PtpcnlzuT6Skg5AlwM91Yyz6l3o_?usp=sharing")
 });
 
 $("button.btn-outline-dark").click(function goto(){
